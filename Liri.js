b229a0733dc55819914c6fcd74cfeaa15ebec337
Manny <manueltrujillo_02@outlook.com>
2019-03-18 16:14:11 -0700

    added spotify

---
+++ Liri.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+var Spotify = require('node-spotify-api');
+
+
+
+var spotify = new Spotify({
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+  });
+   
+  spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+   
+  console.log(data); 
+  });
