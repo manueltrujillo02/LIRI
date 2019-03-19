7bc79d77cb35655cb2af85cd186f536fd1001e24
Manny <manueltrujillo_02@outlook.com>
2019-03-18 17:59:24 -0700

    Update Liri.js

--- Liri.js
+++ Liri.js
@@ -3,10 +3,11 @@ var Spotify = require('node-spotify-api');
 
 
 
-var spotify = new Spotify({
-    id: process.env.SPOTIFY_ID,
-    secret: process.env.SPOTIFY_SECRET
-  });
+var spotify = new Spotify(
+    keys.spotify
+    // id: process.env.SPOTIFY_ID,
+    // secret: process.env.SPOTIFY_SECRET
+  );
    
   spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
     if (err) {
