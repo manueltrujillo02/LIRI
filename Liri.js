6e658c6ae369918bd6da25f87db16403a661c516
Manny <manueltrujillo_02@outlook.com>
2019-03-18 20:02:07 -0700

    Update Liri.js

--- Liri.js
+++ Liri.js
@@ -1,7 +1,15 @@
-require('dotenv').config()
+require('dotenv').config();
 var Spotify = require('node-spotify-api');
 var keys = require("./keys.js");
-var command = process.argv[2]
+var axios = require("axios");
+var command = process.argv[2];
+
+axios 
+  .get("https://rest.bandsintown.com/artists/" + command + "/events?app_id=codingbootcamp")
+  .then(function(response){
+    console.log(response);
+    // console.log(JSON.stringify(response, null,2)); 
+  });
 
 
 var spotify = new Spotify(keys.spotify);
@@ -15,7 +23,7 @@ var spotify = new Spotify(keys.spotify);
       return console.log('Error occurred: ' + err);
     }
    
-  console.log(JSON.stringify(data.artists, null,2)); 
+  // console.log(JSON.stringify(data.artists, null,2)); 
   // console.log(JSON.stringify(data.name, null,2)); 
   // console.log(JSON.stringify(, null,2)); 
   });
