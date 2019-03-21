5a99bfeb5a834a42f7c7937324fdbbe2c11d9c2e
Manny <manueltrujillo_02@outlook.com>
2019-03-20 23:52:42 -0700

    done

--- Liri.js
+++ Liri.js
@@ -2,64 +2,92 @@ require('dotenv').config();
 const keys = require("./keys");
 const Spotify = require('node-spotify-api');
 const spotify = new Spotify(keys.spotify);
+const fs = require("fs");
+const axios = require("axios");
 
-var axios = require("axios");
 
 
-// if(ProgressEvent.argv[2] === "write"){
-//  false.writeFile("random.text", process.argv[3], function(err){
-//    if(err) throw err;
-//    console.log("wrote a file");
-//  });
-// }
+function concert() {
+  const artist = process.argv[3];
+  axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(function (response) {
+    console.log("----------------------------------");
+
+    console.log(response.data);
+    // console.log(response.request);
+    console.log("----------------------------------");
+    // console.log(response.headers);
+  });
+};
 
 function movieThis() {
-  // with the command of movie-this <title> get data from the 
-  // omdb API and show it in the console
-  // node app.js movie-this "Titanic"
+
   const movieTitle = process.argv[3];
-  axios.get("http://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&apikey=trilogy")
-      .then(function (response) {
-        console.log("----------------------------------");
-          console.log("Title: " + response.data.Title);
-          console.log("Year: " + response.data.Year);
-          console.log("Rating: " + response.data.imdbRating);
-          console.log("Plot: " + response.data.Plot);
-          console.log("Country: " + response.data.Country);
-          console.log("Language: " + response.data.Language);
-          console.log("Actors: " + response.data.Actors);
-          
-          
-          console.log("----------------------------------");
-      });
+  axios.get("http://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&apikey=trilogy").then(function (response) {
+    console.log("----------------------------------");
+    console.log("Title: " + response.data.Title);
+    console.log("Year: " + response.data.Year);
+    console.log("Rating: " + response.data.imdbRating);
+    console.log("Plot: " + response.data.Plot);
+    console.log("Country: " + response.data.Country);
+    console.log("Language: " + response.data.Language);
+    console.log("Actors: " + response.data.Actors);
+    console.log("----------------------------------");
+  });
 }
 function spotifyThisSong() {
-  const songTitle = process.argv[3];
+  var songTitle = process.argv[3];
   spotify.search({ type: 'track', query: songTitle }, function (err, data) {
-      if (err) {
-          return console.log('Error occurred: ' + err);
-      }
-      for (let i = 0; i < data.tracks.items.length; i++) {
-          const artistData = data.tracks.items[i];
-          console.log("----------------------------------");
-          console.log("Artist: " + artistData.artists[0].name);
-          console.log("Song Title: " + artistData.name);
-          console.log("Preview URL: " + artistData.preview_url);
-      }
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+    for (let i = 0; i < data.tracks.items.length; i++) {
+      const artistData = data.tracks.items[i];
+      console.log("----------------------------------");
+      console.log("Artist: " + artistData.artists[0].name);
+      console.log("Song Title: " + artistData.name);
+      console.log("Preview URL: " + artistData.preview_url);
+    }
   });
 }
 const command = process.argv[2];
 
 switch (command) {
-    case "write":
-        writeFile();
-        break;
-    case "movie-this":
-        movieThis();
-        break;
-    case "spotify-this-song":
-        spotifyThisSong();
-        break;
-    default: 
-        console.log("Please pick either write, movie-this, or spotify-this-song");
-}
+  case "concert-this":
+    concert();
+    break;
+  case "do-what-it-says":
+    writeFile();
+    break;
+  case "movie-this":
+    movieThis();
+    break;
+  case "spotify-this-song":
+    spotifyThisSong();
+    break;
+  default:
+    console.log("Please pick either concert-this, movie-this, or spotify-this-song");
+};
+
+
+
+function writeFile() {
+  var random = fs.readFileSync("random.text", "utf8");
+  
+  var songTitle = random;
+
+  spotify.search({ type: 'track', query: songTitle }, function (err, data) {
+    console.log("----------------------------------");
+    console.log("Artist: " + data.tracks.items[0].artists[0].name);
+    console.log("Song Title: " + data.tracks.items[0].name);
+    console.log("Preview URL: " + data.tracks.items[0].preview_url);
+   
+    
+    
+    console.log("----------------------------------");
+    
+    
+  });
+
+
+
+};
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# LIRI
+                                             # LIRI
+In this app you will able to give "Liri" a set of commands. Liri will be able to give you a response based on the commands that "she" received. 
+            *available commands*
+                -concert-this "name of artist"
+                -spotify-this-song "song name"
+                -movie-this "title of movie"
+                -do-what-it-says
+
+
+
+
+<!-- Pseudocode -->
+<!-- concert-this -->
+<!--download and install axios  -->
+<!-- require axios const axios = require("axios") -->
+<!-- create a function that gets data from the api using axios -->
+<!-- get command from terminal using process.argv[3] -->
+<!-- do a .then and console.log response -->
+<!-- find the data we need and console.log it || example console.log(response.data.event) -->
